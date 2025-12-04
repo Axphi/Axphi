81e52e93a3297aab1f054d8a613e2f22178d4b74
ilyfairy <me@ilyfairy.com>
2025-12-04 21:45:20 +0800

    fix: 修复保存功能

--- Axphi/Services/ProjectManager.cs
+++ Axphi/Services/ProjectManager.cs
@@ -1,46 +1,41 @@
-﻿using Axphi.Data;
+using Axphi.Data;
 using Axphi.Extensions;
 using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
 using System.IO.Packaging;
-using System.Linq;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 
-namespace Axphi.Services
+namespace Axphi.Services;
+
+public partial class ProjectManager : ObservableObject
 {
-    public partial class ProjectManager : ObservableObject
+    [ObservableProperty]
+    private Project _editingProject = new Project()
     {
-        [ObservableProperty]
-        private Project _editingProject = new Project()
+        Chart = new Chart()
+    };
+
+    [ObservableProperty]
+    private string? _editingProjectFilePath;
+
+    public void SaveProject(Project project, string path)
+    {
+        using var package = Package.Open(path, System.IO.FileMode.Create);
+        var chartJson = JsonSerializer.Serialize(project.Chart);
+        var chartJsonUtfBytes = Encoding.UTF8.GetBytes(chartJson);
+
+        package.WritePartAllBytes("/chart.json", "application/json", chartJsonUtfBytes);
+        package.WritePartAllBytes("/audio", "audio", project.EncodedAudio);
+        package.WritePartAllBytes("/illustration", "audio", project.EncodedIllustration);
+    }
+
+    public void SaveEditingProject(string path)
+    {
+        if (EditingProject is null)
         {
-            Chart = new Chart()
-        };
-
-        [ObservableProperty]
-        private string? _editingProjectFilePath;
-
-        public void SaveProject(Project project, string path)
-        {
-            var package = Package.Open(path, System.IO.FileMode.Create);
-            var chartJson = JsonSerializer.Serialize(project.Chart);
-            var chartJsonUtfBytes = Encoding.UTF8.GetBytes(chartJson);
-
-            package.WritePartAllBytes("/chart.json", "application/json", chartJsonUtfBytes);
-            package.WritePartAllBytes("/audio", "audio", project.EncodedAudio);
-            package.WritePartAllBytes("/illustration", "audio", project.EncodedIllustration);
+            throw new InvalidOperationException("No project editing");
         }
 
-        public void SaveEditingProject(string path)
-        {
-            if (EditingProject is null)
-            {
-                throw new InvalidOperationException("No project editing");
-            }
-
-            SaveProject(EditingProject, path);
-        }
+        SaveProject(EditingProject, path);
     }
 }

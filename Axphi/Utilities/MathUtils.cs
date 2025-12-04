b6ab5df79df5d5ebf5d10a3beab28b99d728ff59
ilyfairy <me@ilyfairy.com>
2025-12-04 21:45:10 +0800

    泛型Lerp

--- Axphi/Utilities/MathUtils.cs
+++ Axphi/Utilities/MathUtils.cs
@@ -1,24 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
-namespace Axphi.Utilities
-{
-    internal static class MathUtils
-    {
-        public static double Lerp(double start, double end, double t)
-        {
-            return start + (end - start) * t;
-        }
+namespace Axphi.Utilities;
 
-        public static Vector Lerp(Vector start, Vector end, double t)
-        {
-            return new Vector(
-                Lerp(start.X, end.X, t),
-                Lerp(start.Y, end.Y, t));
-        }
+internal static class MathUtils
+{
+    public static T Lerp<T>(T start, T end, double t) where T : 
+        System.Numerics.ISubtractionOperators<T, T, T>,
+        System.Numerics.IMultiplyOperators<T, double, T>,
+        System.Numerics.IAdditionOperators<T, T, T>
+    {
+        return start + (end - start) * t;
+    }
+
+    public static Vector Lerp(Vector start, Vector end, double t)
+    {
+        return new Vector(
+            Lerp(start.X, end.X, t),
+            Lerp(start.Y, end.Y, t));
     }
 }
